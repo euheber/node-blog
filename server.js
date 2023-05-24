2a913dac2d9784e5a799f092f3e7f72d41a8f279
euheber <euheber1@gmail.com>
2023-05-24 10:45:58 -0300

    feat: new route for seach based on categories

--- server.js
+++ server.js
@@ -3,43 +3,65 @@ const categoriesControler = require("./categories/categoriesControler")
 const articlesControler = require("./articles/articlesControler")
 const Article = require("./articles/Article")
 const Category = require("./categories/Category")
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = 3000
 
 connection
-.authenticate()
-.then(() => console.log('Conexão com o servidor executada com sucesso'))
-.catch(err => console.log(err))
+  .authenticate()
+  .then(() => console.log("Conexão com o servidor executada com sucesso"))
+  .catch((err) => console.log(err))
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs")
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:false}))
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use("/", categoriesControler)
 app.use("/", articlesControler)
 
-
-app.get('/', (req, res) => {
-    Article.findAll().then( articles => { 
-        res.render('index', {articles})
+app.get("/", (req, res) => {
+  Article.findAll().then((articles) => {
+    Category.findAll().then((categories) => {
+      res.render("index", { articles, categories })
     })
+  })
 })
 
 app.get("/ler/:slug", (req, res) => {
-    const slug = req.params.slug
+  const slug = req.params.slug
 
-    if(slug != undefined) {
-        Article.findOne({where: { 
-            slug:slug
-        }}).then(article => { 
-            res.render("admin/articles/article", {article})
-        })
-    } else { 
-        res.redirect("/index")
-    }
+  if (slug != undefined) {
+    Article.findOne({
+      where: {
+        slug: slug,
+      },
+    }).then((article) => {
+      Category.findAll().then((categories) => {
+        res.render("admin/articles/article", { article, categories })
+      })
+    })
+  } else {
+    res.redirect("/index")
+  }
 })
 
+
+
+app.get("/category/:slug", (req, res) => { 
+  const slug = req.params.slug
+
+  Category.findOne({where: { 
+    slug: slug
+  }, include: [{model:Article}]}).then(category => { 
+    if(category != undefined) { 
+      Category.findAll().then(categories => { 
+        res.render("index", {articles: category.Articles, categories})
+      })
+    } else { 
+      res.redirect("/")
+    }
+  })
+})
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}!`))
